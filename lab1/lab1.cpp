349b2fa05856c6dd7febca5304f8c2701f0af959
vect0r <vectoreddd@gmail.com>
2024-10-10 23:42:56 +0200

    mergesort added

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -9,6 +9,7 @@ wystarczy na std::vector, ale musi by szablonem dzialajacym dla roznych typow.
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 
 template <typename T>
 void bubbleSort(T& arr , int start, int end) {
@@ -27,19 +28,78 @@ void bubbleSort(T& arr , int start, int end) {
     }
 }
 
+template <typename T>
+void merge(T& arr, int left, int middle, int right){
+    std::vector<typename T::value_type> temp(right - left + 1); // tworzymy tymczasowy vector elementów typu T
+    int left1 = left;
+    int right1 = middle;
+    int left2 = middle + 1;
+    int right2 = right;
+    int i = 0;
 
+    while(left1 <= right1 && left2 <= right2){
+        if(arr[left1] <= arr[left2]){
+            temp[i] = arr[left1];
+            left1++;
+        } else {
+            temp[i] = arr[left2];
+            left2++;
+        }
+        i++;
+    }
+
+    while(left1 <= right1){
+        temp[i] = arr[left1];
+        left1++;
+        i++;
+    }
+
+    while(left2 <= right2){
+        temp[i] = arr[left2];
+        left2++;
+        i++; 
+    }
+
+    for(int j = 0; j < temp.size(); j++){
+        arr[left + j] = temp[j];
+    }
+};
+
+
+
+template <typename T>
+void mergeSort(T& arr, int left, int right){
+    if(left < right){
+        int middle = (left + right) / 2;
+        mergeSort(arr, left, middle);
+        mergeSort(arr, middle + 1, right);
+        merge(arr, left, middle, right);
+    }
+
+};
+
+template <typename T>
+void printArr(T& arr){
+    for(auto i: arr){
+        std::cout << i << " ";
+    }
+    std::cout << "\n";
+}
 
 int main() {
-    std::vector<int> v1 = { 9,4,7,3,6,8,2,1,5,0 };
-    std::vector<float> v2 = { 9.0, 4.0, 7.0, 3.0, 6.0, 8.0, 2.0, 5.0, 1.0, 0.0};
-    int arr1[10] = { 9,4,7,3,6,8,2,1,5,0 };
+    std::vector<int> v = { 9,4,7,3,6,8,2,1,5,0 };
+    int arr[10] = { 9,4,7,3,6,8,2,1,5,0 };
 
-    bubbleSort(v1, 0, v1.size());
-    bubbleSort(arr1, 0, 10);
+    //bubbleSort(v1, 0, v1.size());
+    //bubbleSort(arr1, 0, 10);
+    mergeSort(v, 0, v.size() - 1);
+    std::cout << "Result of using mergeSort() on a vector:" << "\n";
+    printArr(v);
+    
+    bubbleSort(arr, 0, 10);
+    std::cout << "Result of using bubbleSort() on an array:" << "\n";
+    printArr(arr);
+    
 
-
-    for(auto i: arr1){
-        std::cout << i << "\n";
-    }
     return 0;
 }
